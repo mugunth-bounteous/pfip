4164e784766cbee629b0553e10e85be99e70c818
mugunth <jc.mugunth@gmail.com>
2023-11-06 11:00:29 +0530

    inital commit

---
+++ backend/src/test/kotlin/com/pfip/backend/BackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.pfip.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
